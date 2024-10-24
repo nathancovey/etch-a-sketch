4908662e050a6c09dcd98e64f29a28593abd95cb
Nathan Covey <156732802+nathancovey@users.noreply.github.com>
2024-10-24 18:48:57 -0400

    add foundational logic

--- game.js
+++ game.js
@@ -1,13 +1,24 @@
 const container = document.querySelector('.container')
-const newSquare = document.createElement('div')
 
-for (let i = 0; i < 16; i++) {
-    newSquare.classList.add('square')
-    container.appendChild(newSquare)
+let squaresChoice = 44
+const containerWidth = 960
+
+const getSquaresChoice = function()
+
+for (let i = 0; i < squaresChoice * squaresChoice; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    const squareSize = containerWidth / squaresChoice
+    square.style.width = `${squareSize}px`
+    square.style.height = `${squareSize}px`
+
+    container.appendChild(square)
+
+    square.addEventListener('mouseenter', () => {
+        square.style.backgroundColor = 'white'
+    })
 }
-
-
-
 
 
 
