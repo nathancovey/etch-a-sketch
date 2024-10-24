7e4909ebaa4195c82ed4bff95204ee8dceb33a94
Nathan Covey <156732802+nathancovey@users.noreply.github.com>
2024-10-24 18:22:01 -0400

    declare square globally

--- game.js
+++ game.js
@@ -1,9 +1,13 @@
 const container = document.querySelector('.container')
+const newSquare = document.createElement('div')
 
 for (let i = 0; i < 16; i++) {
-    const newSquare = document.createElement('div')
     newSquare.classList.add('square')
     container.appendChild(newSquare)
 }
+
+
+
+
 
 
